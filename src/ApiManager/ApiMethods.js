40b04aad541f7d7f9006fea9b341fe360deea537
Italo Costa <italo.costa99@gmail.com>
2024-10-06 15:54:20 -0300

    feat(): .env added to the system

--- src/ApiManager/ApiMethods.js
+++ src/ApiManager/ApiMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 class ApiMethods {
   static async apiRequest(method, url, data = null) {
