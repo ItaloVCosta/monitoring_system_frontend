38f96d133f613edd3ad7ec89b23fe7a05dbc0d45
Italo Costa <italo.costa99@gmail.com>
2024-10-06 02:26:30 -0300

    feat(): filter implemented, react query implemented and comunication with backEnd implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
+import ApiManager from "./ApiManager/ApiManager";
 import "./App.css";
 import Dropdown from "./components/DropDown";
 import SearchInput from "./components/SearchInput";
 import ServerCard from "./components/ServerCard";
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { CiServer } from "react-icons/ci";
+import { useQuery, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const [selectedOption, setSelectedOption] = useState("All Servers");
   const [searchText, setSearchText] = useState("");
 
+  const { data: servers, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: ApiManager.getServers,
+  });
+
+
   const handleSearchChange = (value) => {
     setSearchText(value);
   };
+
   const handleDropdownChange = (value) => {
     setSelectedOption(value);
   };
 
+  const filteredServers = useMemo(() => {
+    return servers?.filter((server) => {
+      const matchesSearchText = server.name.toLowerCase().includes(searchText.toLowerCase());
+      const matchesStatus =
+        selectedOption === "All Servers" ||
+        (selectedOption === "Active Servers" && server.status === 1) ||
+        (selectedOption === "Inactive Servers" && server.status === 0);
+
+      return matchesSearchText && matchesStatus;
+    });
+  }, [servers, searchText, selectedOption]);
+
   return (
-    <div className="bg-gray-900 min-h-screen flex items-center p-4 flex-col ">
+    <div className="bg-gray-900 min-h-screen flex items-center p-4 flex-col">
       <div className="w-4/5">
         <h1 className="text-gray-400 font-bold self-start">Servers</h1>
         <div className="flex flex-col md:flex-row items-center justify-between pb-4 w-full space-y-4 md:space-y-0 md:space-x-4">
@@ -32,7 +54,6 @@ function App() {
               onChange={handleSearchChange}
             />
           </div>
-
           <div className="flex justify-end w-full md:w-auto">
             <button className="bg-slate-700 text-gray-300 rounded-md p-2 flex items-center space-x-2 hover:bg-slate-600 ml-auto">
               <CiServer />
@@ -40,19 +61,29 @@ function App() {
             </button>
           </div>
         </div>
-
-        <ServerCard
-          name="fathomless-silence"
-          version="PHP 8.3, MySQL 8"
-          ip="142.93.62.191"
-          status={true} 
-        />
-        <ServerCard
-          name="lonely-peak"
-          version="Node.js 14, MongoDB"
-          ip="192.168.1.25"
-          status={false} 
-        />
+        
+        {isLoading ? (
+          <div className="flex items-center justify-center h-full w-full">
+            <LoadingSpinner />
+          </div>
+        ) : error ? (
+          <p className="text-red-500">
+            Failed to load servers: {error.message}
+          </p>
+        ) : (
+          <div>
+            {filteredServers?.map((server) => (
+              <ServerCard
+                key={server.id}
+                name={server.name}
+                ip={server.ip_address}
+                status={server.status}
+                memoryUsage={server.memory_usage}
+                cpuUsage={server.cpu_usage}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/ServerCard.jsx
+++ src/components/ServerCard.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const ServerCard = ({ name, version, ip, status }) => {
+const ServerCard = ({ name, ip, status, cpuUsage, memoryUsage }) => {
   return (
-    <div className="bg-slate-800 text-white rounded-lg p-4 flex items-center justify-between shadow-lg flex-col sm:flex-row border-2 border-white border-slate-700 mt-2">
+    <div className="bg-slate-800 text-white rounded-lg p-4 flex items-center justify-between shadow-lg flex-col sm:flex-row border-2 border-slate-700 mt-2">
       <div className="flex items-center space-x-4">
         <div>
-          <p className={`font-semibold text-lg ${status ? 'text-emerald-400' : 'text-red-400'} `}>{name}</p>
-          <p className="text-sm text-gray-400">{version}</p>
+          <p
+            className={`font-semibold text-lg ${
+              status ? "text-emerald-400" : "text-red-400"
+            } `}
+          >
+            {name}
+          </p>
         </div>
       </div>
 
-      <div className="flex items-center space-x-6">
-        <span className="text-sm text-gray-400">{ip}</span>
+      <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-6">
+        <span className="text-sm text-gray-400">IP: {ip}</span>
+        <span className="text-sm text-gray-400">CPU: {cpuUsage}%</span>
+        <span className="text-sm text-gray-400">Memory: {memoryUsage}%</span>
       </div>
 
-      <div className={`flex items-center space-x-2 rounded-full p-2 ${status ? 'bg-emerald-600' : 'bg-red-500'}`}>
-        <span className={`w-3 h-3 rounded-full ${status ? 'bg-green-500' : 'bg-red-400'}`}></span>
-        <span className="text-xs text-white">{status ? 'Connected' : 'Disconnected'}</span>
+      <div
+        className={`flex items-center space-x-2 rounded-full p-2 ${
+          status ? "bg-emerald-600" : "bg-red-500"
+        }`}
+      >
+        <span
+          className={`w-3 h-3 rounded-full ${
+            status ? "bg-green-500" : "bg-red-400"
+          }`}
+        ></span>
+        <span className="text-xs text-white">
+          {status ? "Connected" : "Disconnected"}
+        </span>
       </div>
     </div>
   );
